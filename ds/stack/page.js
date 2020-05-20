28ee6d3ee6915c8babb13a27861dd506193f3eb4
DL1683 <dl1683@rit.edu>
2020-05-20 15:26:20 +0530

    Finished the Stack.

--- ds/stack/page.js
+++ ds/stack/page.js
@@ -1,6 +1,7 @@
 // define the stack
 var stack = new Stack();
 
+
 function addCol(value) {
 	var t = document.getElementById("row");
 	var tr = document.createElement("tr");
@@ -15,36 +16,41 @@ function addCol(value) {
 
 var defaultValueCounter = 0;
 function push() {
-	stack=new Stack();
 	var value = document.getElementById("add").value;
-	if (value === "") {
+	if (value === "" || value == NaN || value == 'NaN') {
 		defaultValueCounter++;
-		value = defaultValueCounter;
+		value = 0;
+	} else {
+		console.log("Pushing" + ", " + value)
+		stack.push(value);
+		addCol(value);
 	}
-	console.log(stack+", "+value)
-	stack.push(value);
-	addCol(value);
-
+	//stack.print();
 	document.getElementById("add").value = "";
+
 }
 
 function pop() {
-	stack=new Stack();
-	setTimeout(function() {
+	//stack=new Stack();
+	setTimeout(function () {
 		try {
 			var value = stack.pop();
+			console.log(stack + ", " + value);
 			removeCol(value);
 		} catch (e) {
-			showMessage("#status", "Stack Underflow", "red")
+			showMessage("#status", "Underflow", "red")
 		}
 	}, 200)
 }
 
-function top() {
-	stack=new Stack();
-	setTimeout(function() {
+function peek() {
+	//stack=new Stack();
+	setTimeout(function () {
 		try {
 			var value = stack.top();
+			console.log(value)
+			//alert("Top is: " +value)
+			highlightCol(value, "")
 		} catch (e) {
 			showMessage("#status", "No top", "red")
 		}
@@ -55,17 +61,20 @@ function top() {
 
 function highlightCol(value, css) {
 	$("#" + value).animate({
-		top : "20px"
+		top: "20px"
 	});
-	$("#" + value).find('td').addClass(css);
+	setTimeout(function () {
+		$("#" + value).find('td').addClass("hightlight");
+		showMessage("#status", "Finished", "green")
+	}, 500);
 }
 
 function removeCol(value) {
 	$("#" + value).animate({
-		top : "20px"
+		top: "20px"
 	});
 	$("#" + value).find('td').addClass("focus");
-	setTimeout(function() {
+	setTimeout(function () {
 		$("#" + value).fadeOut(500).delay(1000).remove();
 		showMessage("#status", "Finished", "green")
 	}, 500);
--- ds/stack/stack.js
+++ ds/stack/stack.js
@@ -1,12 +1,59 @@
+
 function Node() {
 	this.value;
 	this.next ;
 }
 
 var Stack= function(){
-	this.min;
-	this.max;
 	this.head;
+}
+
+Stack.prototype.push=function(value) {
+	if(value==undefined || value==""|| value==NaN){
+		alert ("Please input proper value (number)")
+	}
+	if(this.head==undefined||this.head==NaN){//nothing exists yet
+		this.head=new Node();
+		this.head.value=value;
+		this.head.next=new Node();
+	}else{//nonempty stack
+		var c=new Node;
+		c.next = this.head;
+		c.value=value;
+		this.head=c;
+	}	
+}
+
+Stack.prototype.print=function(){
+	//debugging purposes
+	var c=new Stack();
+	c.head=this.head
+	while(c.head!=undefined || c.head.value!=NaN){
+		console.log("Val: "+c.head.value)
+		c=c.head.next
+	}
+	console.log("Head: "+ this.head.value)
+}
+
+Stack.prototype.pop=function() {
+	console.log("pop")
+	if(this.head==undefined){//nothing exists yet
+		alert ("Trying to pop null")
+	}else{//nonempty stack
+		var val=this.head.value;
+		this.head=this.head.next;
+		return val;
+	}	
+}
+
+Stack.prototype.top=function() {
+	console.log("top")
+	if(this.head==undefined){//nothing exists yet
+		alert ("Trying to get top of null")
+	}else{//nonempty stack
+		var val=this.head.value;
+		return val;
+	}	
 }
 
 Node.prototype.insert=function(value) {
@@ -21,39 +68,4 @@ Node.prototype.insert=function(value) {
 	}
 	var c = current.next;
 	c.insert(value);
-}
-
-Stack.prototype.push= function(value) {
-	if(value==undefined || value==""){
-		throw "Please input proper value (number)"
-	}
-	if(this.head==undefined){//nothing exists yet
-		this.head=new Node();
-		this.head.value=value;
-	}else{//nonempty stack
-		var c=this.head;
-		c.next=this.head;
-		c.value=value;
-		this.head=c
-	}	
-}
-
-
-Stack.prototype.top= function() {	
-	if(this.head==undefined){//nothing exists yet
-		throw "Trying to get top of null"
-	}else{//nonempty stack
-		return this.head.value;
-	}	
-}
-
-Stack.prototype.pop= function() {
-	
-	if(this.head==undefined){//nothing exists yet
-		throw "Trying to get top of null"
-	}else{//nonempty stack
-		var val=this.head.value;
-		this.head=this.head.next;
-		return val;
-	}	
 }
